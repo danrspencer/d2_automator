9737bddbf61503fce6737ca9291568b248c35dc5
Dan Spencer <dan@spencermail.me>
2024-01-26 23:42:55 +0000

    Detect if the URL has a querystring in it to stop infinite redirects

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,53 @@
-mod utils;
 mod bungie;
 mod config;
 mod dim;
+mod utils;
 
-use wasm_bindgen::prelude::*;
+use js_sys::Promise;
 use oauth2::AuthorizationCode;
+use reqwest::Url;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::future_to_promise;
 
 use crate::{bungie::oauth::OAuth, config::Config};
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
-
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    let config = Config::new();
+pub fn greet() -> Promise {
+    future_to_promise(async move {
+        let config = Config::new();
 
-    let mut client = OAuth::init_oauth_client(&config);
-    let auth_url = client.generate_auth_url();
+        let mut client = OAuth::init_oauth_client(&config);
 
-    
-    web_sys::window().unwrap().open_with_url_and_target(auth_url.as_str(), "_self").unwrap();
-        
-        // .set_href(auth_url.as_str())
-        // .unwrap();
+        // get the url via web_sys
+        let href = web_sys::window().unwrap().location().href().unwrap();
+        let url = Url::parse(href.as_str()).unwrap();
+        if let Some(code) = url
+            .query_pairs()
+            .find(|(key, _)| key == "code")
+            .map(|(_, value)| value.to_string())
+        {
+            let code = AuthorizationCode::new(code.to_string());
+            let token = client.exchange_code(code).await.unwrap();
+
+            alert(format!("Access token: {}", token).as_str());
+        } else {
+            let auth_url = client.generate_auth_url();
+
+            web_sys::window()
+                .unwrap()
+                .open_with_url_and_target(auth_url.as_str(), "_self")
+                .unwrap();
+        }
+
+        Ok(JsValue::UNDEFINED)
+    })
+
+    // .set_href(auth_url.as_str())
+    // .unwrap();
     // alert(format!("Hello, {}", auth_url).as_str());
 }
