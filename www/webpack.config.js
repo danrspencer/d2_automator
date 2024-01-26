3d0f4362a3ddfe04212888f77f75837195701c61
Dan Spencer <dan@spencermail.me>
2024-01-26 23:14:43 +0000

    Added https to webpack dev server

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,5 +1,6 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const fs = require('fs');
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -11,4 +12,14 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+  devServer: {
+
+    compress: true,
+    port: 8080, // You can specify your port here
+    https: {
+      key: fs.readFileSync(path.join(__dirname, 'localhost.key')), // Path to your key
+      cert: fs.readFileSync(path.join(__dirname, 'localhost.crt')), // Path to your cert
+    },
+    open: true // Open browser after server had been started
+  },
 };
